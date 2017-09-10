ca8bde8a7750d4ee229dc8502772a633d0f6cbea
Ivan Gonzalez <hi@dreamingecho.es>
2017-09-10 22:43:48 +0200

    Update mix.exs with new configurations and packages

--- mix.exs
+++ mix.exs
@@ -5,12 +5,20 @@ defmodule DiversityInTech.Mixfile do
     [
       app: :diversity_in_tech,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      # ExCoveralls config
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -26,6 +34,7 @@ defmodule DiversityInTech.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:ci),   do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
 
   # Specifies your project dependencies.
@@ -33,6 +42,7 @@ defmodule DiversityInTech.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      # Default Phoenix stuff
       {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
@@ -40,7 +50,22 @@ defmodule DiversityInTech.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+
+      # Authentication
+      {:comeonin, "~> 4.0"},
+      {:bcrypt_elixir, "~> 1.0", override: true},
+      {:guardian, "~> 0.14.0"},
+
+      # Code analysis
+      {:credo, "~> 0.8", only: [:dev, :test]},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+
+      # Code documentation
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+
+      # Testing
+      {:excoveralls, "~> 0.6", only: [:test, :ci]}
     ]
   end
 
