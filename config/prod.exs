7b7c7f22db8a3fdab390fe28bd72fe4436a4eb33
Ivan Gonzalez <hi@dreamingecho.es>
2017-09-17 00:05:04 +0200

    Remove import of prod.secret.exs file in prod config

--- config/prod.exs
+++ config/prod.exs
@@ -58,7 +58,3 @@ config :logger, level: :info
 #
 #     config :diversity_in_tech, DiversityInTechWeb.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
