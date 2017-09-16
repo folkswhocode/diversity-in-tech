be9f72d9c674d0f12781c91d5d3c79a17e648ebe
Ivan Gonzalez <hi@dreamingecho.es>
2017-09-16 23:48:29 +0200

    Add new admin scope for the admin panel

--- lib/diversity_in_tech_web/router.ex
+++ lib/diversity_in_tech_web/router.ex
@@ -19,8 +19,16 @@ defmodule DiversityInTechWeb.Router do
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", DiversityInTechWeb do
-  #   pipe_through :api
-  # end
+  # Admin scope
+    scope "/admin", DiversityInTechWeb.Admin, as: :admin do
+      pipe_through [:browser, :browser_session]
+
+      # Admin unauthenticated scope
+      scope "/" do
+      end
+
+      # Admin authenticated scope
+      scope "/" do
+      end
+    end
 end
