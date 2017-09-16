46b5ebf6558caf7b53eb9e204d2a8aa51fd27d36
Ivan Gonzalez <hi@dreamingecho.es>
2017-09-17 00:12:37 +0200

    Update production environment configuration

--- config/prod.exs
+++ config/prod.exs
@@ -21,40 +21,12 @@ config :diversity_in_tech, DiversityInTechWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :diversity_in_tech, DiversityInTechWeb.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [:inet6,
-#               port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :diversity_in_tech, DiversityInTechWeb.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
+# Configure your database
+config :hcmex, DiversityInTech.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :diversity_in_tech, DiversityInTechWeb.Endpoint, server: true
-#
+config :guardian, Guardian,
+  secret_key: System.get_env("SECRET_KEY_BASE")
